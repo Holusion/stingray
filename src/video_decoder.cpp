Holusion/stingray

commit 9a634ffeef2ee52e928dea9a7d8f3df5a7803956

add link to stingray doc in README

src/video_decoder.cpp
@@ -58,7 +58,9 @@ void  VideoDecoder::decode(unsigned int current) {
   //! Seek if we are not on the right frame
   if (m_context.nextFrame != current)
     seek(current);
-
+  if ( 10 < m_context.nextFrame){
+    seek(0);
+  }
   //! Fill decodeArray with VideoFrame
   for (unsigned int i = 0; i < DECODE_SIZE; ++i) {
 

commit 3055fac7b519792d6c22a3de0b9ee0c4c55815e6

remove bug introduced in 9a634ffeef2ee52e928dea9a7d8f3df5a7803956

src/video_decoder.cpp
@@ -58,9 +58,7 @@ void  VideoDecoder::decode(unsigned int current) {
   //! Seek if we are not on the right frame
   if (m_context.nextFrame != current)
     seek(current);
-  if ( 10 < m_context.nextFrame){
-    seek(0);
-  }
+
   //! Fill decodeArray with VideoFrame
   for (unsigned int i = 0; i < DECODE_SIZE; ++i) {
 