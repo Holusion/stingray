f9444c427a2013aa543d1d587be515b7881f25fd
YannDub <yann59.dubois@gmail.com>
2019-04-05 09:50:16 +0200

    Update test_video because video->buffer change

--- test/test_video.cpp
+++ test/test_video.cpp
@@ -6,10 +6,10 @@ namespace  entities {
 
   TEST_F(VideoTest, OpenVideo) {
     Video video(FIXTURES "/fixtures/goutput.mov",18,19);
-    EXPECT_EQ(video.buffer->index(), 0);
+    EXPECT_EQ(video.buffer.index(), 0);
   }
     TEST_F(VideoTest, OpenAtIndex) {
     Video video(FIXTURES "/fixtures/goutput.mov",18,19, 5);
-    EXPECT_EQ(video.buffer->index(), 5);
+    EXPECT_EQ(video.buffer.index(), 5);
   }
 }
