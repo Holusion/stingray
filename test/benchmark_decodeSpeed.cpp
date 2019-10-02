69d2b159d5a5ccfbd87a342128e521e26c4fdef3
Sebastien Dumetz <s.dumetz@holusion.com>
2019-10-02 17:04:41 +0200

    fix benchmark binary

--- test/benchmark_decodeSpeed.cpp
+++ test/benchmark_decodeSpeed.cpp
@@ -1,49 +1,48 @@
+#include <unistd.h>
+#include  <thread>
+#include  <chrono>
+
 #include "../config.h"
 #include  "debug.h"
 #include  "video_decoder.hpp"
 #include  "video_frame.hpp"
 #include  "exceptions/global_exception.hpp"
 #include  "exceptions/av_exception.hpp"
-#include  <thread>
-#include  <chrono>
+
+#define CYCLE_LENGTH 1000
 
 /*
  * USAGE : benchmark /path/to/video.mp4
  * Will give time taken to decode a chunk of frames.
  * It tests only reverse decode as there is no way forward decode is more problematic...
  */
-void cycle(decoder::VideoDecoder* decoder,entities::Video* video ){
+void cycle(entities::Video* video ){
   int i,c;
   std::chrono::high_resolution_clock::time_point p(std::chrono::high_resolution_clock::now());
 
-  for (c=0;c<10;c++){
-    decoder->decodeAndWrite(video->buffer);
-    for(i=0;i<DECODE_SIZE;i++){
-      //We don't check buffer has enough elements : it should...
-      video->buffer.forward();
-    }
+  for (c=0;c<CYCLE_LENGTH;c++){
+    while(video->buffer.size() == 0)
+      usleep(1);
+    video->buffer.forward();
   }
   using dura = std::chrono::duration<double>;
         auto d = std::chrono::high_resolution_clock::now() - p;
-        std::cout << "Decode cycle: "
-            << std::chrono::duration_cast<dura>(d/10).count()
+        std::cout << "Decode cycle ("<<CYCLE_LENGTH<<" frames): "
+            << std::chrono::duration_cast<dura>(d).count()
             <<" s"
             << std::endl;
 }
 
 int  main (int argc, char** argv) {
 
-  entities::Video        video(argv[1],1920,1080);
-  decoder::VideoDecoder decoder(video.context);
-
   if (argc < 2){
     return EXIT_FAILURE;
   }
-
-  video.buffer.swap();
+  entities::Video        video(argv[1],1920,1080);
+  video.buffer.swap(); //Reverse-decode is usually slower and what we're interested in
   try {
     while(true)
-      cycle(&decoder,&video);
+      cycle(&video);
   }catch (AVException& e){
     std::cerr <<"AV Exception : "<< e.what() <<std::endl;
     return EXIT_FAILURE;
